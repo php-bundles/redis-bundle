6f7c37f9b0766e318feb992a592785511c65c9ba
Dmitry Khaperets <khaperets@gmail.com>
2016-06-02 05:46:08 +0300

    Update phpdoc

--- Service/ClientInterface.php
+++ Service/ClientInterface.php
@@ -8,8 +8,8 @@ interface ClientInterface extends \Predis\ClientInterface
     /**
      * Insert all the specified values at the tail of the list stored at key.
      *
-     * @param string $key
-     * @param ...    $values
+     * @param string                $key
+     * @param string|int|float|bool ...$values
      *
      * @return int The length of the list after the push operation.
      *
